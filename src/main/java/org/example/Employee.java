38dbbf89abd2a0e899643856ce2f07d3d8cb2231
70899 <a1135063213@gmail.com>
2022-10-09 16:20:27 +0800

    第二次修改

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Employee{
+    public String name;
+    public Employee(String name){
+        this.name=name;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+
 public class Main {
+    public static void swap(Employee x,Employee y)
+    {
+        Employee temp = x;
+        x=y;
+        y=temp;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world! my first commits");
+        Employee employee1=new Employee("张三");
+        Employee employee2=new Employee("李四");
+        System.out.println(employee1);
+        System.out.println(employee1.name);
+        System.out.println(employee2);
+        System.out.println(employee2.name);
+
+        swap(employee1, employee2);
+        System.out.println(employee1);
+        System.out.println(employee1.name);
+        System.out.println(employee2);
+        System.out.println(employee2.name);
+
+        Integer i1=100;
+        System.out.println(i1);
+        i1=i1*2;
+        System.out.println(i1);
+
     }
 }
